c81627868c25ac6eef0331006f2752a5e307cc4d
Rin <=>
2019-10-13 06:38:19 +0700

    master change

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -92,14 +92,11 @@
                             </li>
                             <li><a href="/about">About</a></li>
                             <li><a href="/contact">Contact</a></li>
-        
                         </ul>
                         </div>
-                        
                     </div>
                 </nav>
             </header>
-
         @yield('content')
         @show
 
